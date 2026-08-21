46bbf501737bfe1e7dcf00c98be8405dde47d46b
wagnergabrieloliveirabip <wagner.gabriel.oliveira@escola.pr.gov.br>
2026-08-21 10:48:15 -0300

    ok

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", ()=>{
 const likeBtn = document.querySelector(".left-actions .action-btn:first-child");
+if (!likeBtn) return;
+
+const likeSvg =likeBtn.querySelector("svg");
+
+// contador
+let textNode = Array.from(likeBtn.childNodes). find(
+(node) => node.nodeType === Node.TEXT_NODE && node
+);
+
+
+
+
+
+
+
 })
